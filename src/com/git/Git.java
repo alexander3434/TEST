40ce8b8d4931405ad73c3d8d7f4246414e7d8778
alexander3434 <alexander.kozyrev2012@yandex.ru>
2020-03-01 20:02:37 +0300

    Initial commit

---
+++ src/com/git/Git.java
@@ -0,0 +1,13 @@
+package com.git;
+
+public class Git {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+//        $ git config-- global user.name "alexander3434"
+//        $ git config-- global user.email alexander.kozyrev2012 @yandex.ru
+//        $ git config-- global core.editor vim // использую текстовый редактор вим
+//        $ git config-- list  //гит находит находит все настройки
